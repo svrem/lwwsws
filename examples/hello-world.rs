05cfd6f10f707abcb49ad2c1d953a9ba0a24731b
svrem <svrem@icloud.com>
2023-11-04 10:00:40 +0100

    an update to the example

--- examples/hello-world.rs
+++ examples/hello-world.rs
@@ -1,16 +1,20 @@
 use lwwsws::HttpServer;
 
-async fn handler(path: String) -> Option<String> {
-    match path.as_str() {
-        "/" => Some("Hello, World!".to_string()),
-        "/hello" => Some("Wow, this is a new route!".to_string()),
-        _ => None,
-    }
+async fn get_string() -> String {
+    "Hello, World!".to_string()
 }
 
 #[tokio::main]
 async fn main() {
+    let handler = |path: String| async move {
+        match path.as_str() {
+            "/" => Some(get_string().await),
+            "/hello" => Some("Wow, this is a new route!".to_string()),
+            _ => None,
+        }
+    };
+
     let http_server = HttpServer::new(handler);
 
-    http_server.run("127.0.0.1:8080").await
+    http_server.run("127.0.0.1:8080").await;
 }
